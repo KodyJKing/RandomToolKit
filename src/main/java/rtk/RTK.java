3d298fed825e7fe72689279d6d32a881217a9a32
KodyJKing <kody.j.king@gmail.com>
2018-06-03 12:24:37 -0700

    Incremented version for build.

--- src/main/java/rtk/RTK.java
+++ src/main/java/rtk/RTK.java
@@ -21,7 +21,7 @@ public class RTK {
 
     public static final String modId = "rtk";
     public static final String name = "Random Tool Kit";
-    public static final String version = "1.3.1";
+    public static final String version = "1.3.3";
 
     @SidedProxy(clientSide = "rtk.proxy.ClientProxy", serverSide = "rtk.proxy.CommonProxy")
     public static CommonProxy proxy;
