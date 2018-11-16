69442a87c44857c6b33c4f8c79a8c844b0644746
KodyJKing <kody.j.king@gmail.com>
2018-11-16 03:58:43 -0800

    Updated version.

--- src/main/java/rtk/RTK.java
+++ src/main/java/rtk/RTK.java
@@ -27,7 +27,7 @@ public class RTK {
 
     public static final String modId = "rtk";
     public static final String name = "Random Tool Kit";
-    public static final String version = "1.3.9";
+    public static final String version = "1.4";
 
     @SidedProxy(clientSide = "rtk.proxy.ClientProxy", serverSide = "rtk.proxy.CommonProxy")
     public static CommonProxy proxy;
