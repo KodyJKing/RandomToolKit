666334d78354c1e1afed2f90179316b53d9e3f8e
KodyJKing <kody.j.king@gmail.com>
2020-12-29 01:39:26 -0800

    Working on auto generating resource json.

--- generateResources.js
+++ generateResources.js
@@ -1,12 +1,66 @@
-// const fs = require("fs")
-// const path = require("path")
+const fs = require( "fs" )
+const path = require( "path" )
+const assetRoot = "./src/generated/resources/assets/rtk"
 
-// const assetRoot = "./src/main/resources/assets/rtk"
+{
+    generateItemModel( "devtool" )
+    generateBlock( "tent" )
+}
 
-// function output(pathStr, jsonObj) {
-//     fs.writeFileSync(path.path(path), )
-// }
+function output( pathStr, jsonObj ) {
+    let netPath = path.join( assetRoot, pathStr )
+    fs.mkdirSync( path.dirname( netPath ), { recursive: true } )
+    fs.writeFileSync( netPath, JSON.stringify( jsonObj, null, 2 ) )
+}
 
-// function generateBlockItem(name) {
-//     let json = JSON.stringify()
-// }
+function generateBlockItemModel( name ) {
+    output(
+        `models/item/${ name }.json`,
+        {
+            "parent": "rtk:/block/" + name
+        }
+    )
+}
+
+function generateBlockModel( name ) {
+    output(
+        `models/block/${ name }.json`,
+        {
+            "parent": "minecraft:block/cube_all",
+            "textures": {
+                "all": "rtk:block/" + name
+            }
+        }
+    )
+}
+
+function generateBlockState( name ) {
+    output(
+        `blockstates/${ name }.json`,
+        {
+            "variants": {
+                "": {
+                    "model": "rtk:block/" + name
+                }
+            }
+        }
+    )
+}
+
+function generateItemModel( name ) {
+    output(
+        `models/item/${ name }.json`,
+        {
+            "parent": "item/generated",
+            "textures": {
+                "layer0": "rtk:items/" + name
+            }
+        }
+    )
+}
+
+function generateBlock( name ) {
+    generateBlockState( name )
+    generateBlockModel( name )
+    generateBlockItemModel( name )
+}
